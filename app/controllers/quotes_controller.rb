8550f3bc3b031571846b5bf8c53622950f853292
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-26 15:41:21 +0300

    Add edit file

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
     before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
     def index
-        @quotes = Quotes.all
+        @quotes = Quote.all
     end
 
     def new
