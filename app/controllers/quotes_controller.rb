17c62dcec40623d71a89b1d73af3c9a21969bd47
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-26 15:12:30 +0300

    Add quote controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,11 @@
 class QuotesController < ApplicationController
+    before_action :set_quote, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @quotes = Quotes.all
+    end
+
+    def new
+        @quote = Quote.new
+    end
 end
