7ddcd273ef6e3fb892423f45f50e0af01c119358
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-29 21:54:39 +0300

    Add lineitem view

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,5 +1,6 @@
 class LineItemDatesController < ApplicationController
     before_action :set_quote
+    before_action :set_line_item_date, only: [:edit, :update, :destroy]
 
     def new
       @line_item_date = @quote.line_item_dates.build
@@ -15,6 +16,16 @@ class LineItemDatesController < ApplicationController
         end
     end
 
+    def edit
+    end
+    
+    def update
+      if @line_item_date.update(line_item_date_params)
+        redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
     private 
 
     def line_item_date_params
