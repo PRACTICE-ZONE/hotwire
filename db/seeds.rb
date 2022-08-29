c66c1441acb9bdc23637ffdea1e7be33157986d1
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-29 20:40:40 +0300

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,6 @@ manager = User.create!(email: "manager@kk.com", password: "123456", company: gua
 operator = User.create!(email: "operator@pnsolution.com", password: "123456", company: construction)
 engineer = User.create!(email: "engineer@pnsolution.com", password: "123456", company: construction)
 
-quote = Quote.create!(name: "first quote")
+quote = Quote.create!(name: "first quote", company: guard)
 date = LineItemDate.create!(date: Date.current, quote:quote )
 date1 = LineItemDate.create!(date: Date.current + 1.week, quote:quote)
