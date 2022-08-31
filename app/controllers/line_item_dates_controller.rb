2c69f23429620dada9e2d6613a3d2a16cb996abb
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-31 22:05:23 +0300

    Add turbo frame to edit form

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -29,8 +29,10 @@ class LineItemDatesController < ApplicationController
     end
 
     def update
-      if @line_item_date.update(line_item_date_params)
-        redirect_to quote_path(@quote), notice: "Date was successfully updated."
+        respond_to do |format|
+          format.html { redirect_to quote_path(@quote), notice: "Date was successfully updated." }
+          format.turbo_stream { flash.now[:notice] = "Date was successfully updated." }
+        end
       else
         render :edit, status: :unprocessable_entity
       end
