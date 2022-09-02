c8b2aef9f17203ec3b6f1ec28f678b1d233e76b3
IBUNHABIBU <ibunhabib@gmail.com>
2022-09-02 19:52:03 +0300

    Add line item controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,11 +1,19 @@
 class LineItemsController < ApplicationController
   def new
+    @line_item = @line_item_date.line_items.build
   end
 
   def edit
   end
 
   def create
+    @line_item = @line_item_date.line_items.build(line_item_params)
+
+    if @line_item.save
+      redirect_to quote_path(@quote), notice: "Item was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
