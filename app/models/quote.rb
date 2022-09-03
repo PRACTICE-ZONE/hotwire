92d605cfc2f5f5062da28190fcab84f774e6e3bb
IBUNHABIBU <ibunhabib@gmail.com>
2022-09-03 12:36:30 +0300

    Add total price

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,8 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
